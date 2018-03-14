53765ddec27c76f2045801cc946f9b34d5e263b0
Admin <admin@192.168.148.128>
2018-03-14 10:00:34 +0300

    Initial Commit

---
+++ MapboxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapboxTest
+//
+//  Created by Admin on 14.03.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
